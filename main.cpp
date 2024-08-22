8ff9bc197217339a3e06d41c559e28c6e53f026e
hicommit <169062670+hicommit@users.noreply.github.com>
2024-08-22 16:37:41 +0700

    Submit code at 8/22/2024, 5:31:18 PM by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
-    int n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n;
     cin >> n;
 
     long long fib[46];
     fib[0] = 0;
     fib[1] = 1;
 
-    for (int i = 2; i <= n; i++) {
+    for (long long i = 2; i <= n; i++) {
         fib[i] = fib[i - 1] + fib[i - 2];
     }
 
     long long sum = 0;
-    for (int i = 0; i <= n; i++) {
+    for (long long i = 0; i <= n; i++) {
         sum += fib[i];
     }
 
     cout << sum << endl;
+    
     return 0;
 }
