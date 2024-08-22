e035c011fd1174a00619f14d1b93c43165e460b9
hicommit <169062670+hicommit@users.noreply.github.com>
2024-08-22 16:42:38 +0700

    Submit code at 8/22/2024, 5:41:11 PM by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// HICOMMIT TEMPLATE FOR C++
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -7,9 +6,16 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    /*
-        Your code goes here. Happy coding!
-    */
+    long long n;
+    cin >> n;
+
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    cout << sum << endl;
 
     return 0;
 }
