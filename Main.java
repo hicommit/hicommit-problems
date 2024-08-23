73962aa098f9d0bec1c5e432a41f32d5675b2d37
hicommit <169062670+hicommit@users.noreply.github.com>
2024-08-24 00:37:53 +0700

    Submit code at 8/24/2024, 1:36:41 AM by HiCommit

--- Main.java
+++ Main.java
@@ -3,22 +3,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-
-        Arrays.sort(arr);
-
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i]);
-            if (i < n - 1) {
-                System.out.print(" ");
-            }
-        }
     }
 }
