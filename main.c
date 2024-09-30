ee8a2c014e346981238c57022391958c58fb1329
hicommit <169062670+hicommit@users.noreply.github.com>
2024-09-30 15:14:21 +0700

    Nộp lần 5

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main() {
     if(a > 10000 || b > 10000 || a <= 0 || b <= 0 || a < b){
       printf("SAI DU LIEU");
     } else {
-      printf("%f.2", a*b);
+      printf("%.2f", a*b);
     }
   
     return 0;
