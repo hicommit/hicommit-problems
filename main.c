f53e957a21725e3d94305e788b3d347474caafd4
hicommit <169062670+hicommit@users.noreply.github.com>
2024-09-30 15:16:52 +0700

    Test error

--- main.c
+++ main.c
@@ -5,12 +5,12 @@ int main() {
 
     float a, b;
 
-    scanf("%f %f", &a, &b)
+    scanf("%f %f", &a, &b);
 
     if(a > 10000 || b > 10000 || a <= 0 || b <= 0 || a < b){
       printf("SAI DU LIEU");
     } else {
-      printf("%.2f", a*b);
+      printf("%.2f", a*b/0);
     }
   
     return 0;
