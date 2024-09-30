ad9b17dd3e3fae936e34f7ca084e008b1848b39f
hicommit <169062670+hicommit@users.noreply.github.com>
2024-09-30 15:13:06 +0700

    Nộp lần 4

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
   
 int main() {
 
-    int a, b;
+    float a, b;
 
-    scanf("%d %d", &a, &b);
+    scanf("%f %f", &a, &b);
 
     if(a > 10000 || b > 10000 || a <= 0 || b <= 0 || a < b){
       printf("SAI DU LIEU");
     } else {
-      printf("%d", a*b);
+      printf("%f.2", a*b);
     }
   
     return 0;
