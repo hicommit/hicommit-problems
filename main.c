8b8f37fb8e2874e3ca34143088cd4d94398f8d19
hicommit <169062670+hicommit@users.noreply.github.com>
2024-10-05 16:35:03 +0700

    Submit code at 10/5/2024, 4:31:41 PM by HiCommit

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main() {
+  int n, temp, sumOfEven = 0;
+
+  scanf("%d", &n);
+  while(n--){
+    scanf("%d", &temp);
+    sumOfEven += !(temp % 2);
+  }
+
+  printf("%d", sumOfEven);
   return 0;
 }
